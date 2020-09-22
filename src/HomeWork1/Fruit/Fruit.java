430aeb3ffbd604bf3c5bcd576702ee8e4f8a849a
SergeyGrishchuk <sirgeyaid666@gmail.com>
2020-09-22 20:12:11 +0300

    HW.PRO.1

---
+++ src/HomeWork1/Fruit/Fruit.java
@@ -0,0 +1,13 @@
+package HomeWork1.Fruit;
+
+public class Fruit {
+    private float weight;
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public Fruit(float weight) {
+        this.weight = weight;
+    }
+}
---
+++ src/HomeWork1/Fruit/Orange.java
@@ -0,0 +1,7 @@
+package HomeWork1.Fruit;
+
+public class Orange extends Fruit{
+    public Orange(float weight) {
+        super(1.5f);
+    }
+}
